728ea6031ddb07bc19d96aee29e5a55890fe2de4
dodortus <dodortus@gmail.com>
2018-12-05 23:45:13 +0900

    * Update Development detail component

--- src/pages/DevelopmentDetails/ReactEvernote.js
+++ src/pages/DevelopmentDetails/ReactEvernote.js
@@ -5,25 +5,48 @@ class ReactEvernote extends Component {
   render() {
     return (
       <DevelopmentDetailTemplate title="React Evernote">
-        <div className="image-wrap">
+        <section className="image-wrap">
           <img src={require("statics/img/development/thumb-react-evernote.png")} alt="" />
-        </div>
+        </section>
 
         <h2>
           React와 Firebase에 관심을 갖던 시기에 두가지를 활용해서 무언가를 만들어 보고 싶었다.<br />
           그래서 평소에 자주 사용하는 Evernote를 구현해 보기로 하고 조금씩 작업을 시작해 보았다.
         </h2>
-        
-        <div>
-          <a href="#">Live</a> | <a href="#">Github</a>
-        </div>
 
-        <ul>
-          <li>개발기간: 2017.7 ~ 2017.9</li>
-          <li>역할: Frontend 개발, Firebase 스키마 설계 및 연동</li>
-          <li>주요개발항목: </li>
-          <li>사용기술: Javascript(ES6), React, Firebase Realtime DB</li>
-        </ul>
+        <section className="list-wrap">
+          <ul>
+            <li>
+              <strong>개발년도</strong>
+              <p>2017</p>
+            </li>
+            <li>
+              <strong>역할</strong>
+              <p>Frontend 개발, Firebase DB 스키마 설계 및 연동</p>
+            </li>
+            <li>
+              <strong>주요개발항목</strong>
+              <p>노트, 즐겨찾기, 테그 등</p>
+            </li>
+            <li>
+              <strong>사용기술</strong>
+              <p>Javascript(ES6), React, SCSS, Firebase Realtime DB, Webpack</p>
+            </li>
+          </ul>
+        </section>
+
+        <section className="button-wrap">
+          <a href="http://play.codejs.co.kr/react-evernote" className="button" target="_blank">Live</a>
+          <a href="https://github.com/dodortus/react-evernote" className="button" target="_blank">Github</a>
+        </section>
+
+        <section className="more-wrap">
+          <h3>More Projects</h3>
+          <div>
+            <a>A</a>
+            <a>B</a>
+          </div>
+        </section>
       </DevelopmentDetailTemplate>
     );
   }
