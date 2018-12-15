1161138cf9dcfc05b2ef0216503ad88381843624
dodortus <dodortus@gmail.com>
2018-12-15 20:49:58 +0900

    * Update balloon content

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,16 @@ class About extends Component {
     return (
       <PageTemplate>
         About
+
+        박정식<br />
+        프론트앤드 개발자<br />
+        <br /><br />
+
+        저는 알서포트라는 회사에서 프론트앤드 개발자로 일하고 있으며,
+        2010년 부터 프론트앤드 개발자로 다수의 프로젝트를 진행 하고 있습니다.
+        현재 진행중인 프로젝트는 리모트미팅 (WebRTC기반 화상회의 서비스) 입니다.
+
+        이메일, 링크드인?
       </PageTemplate>
     );
   }
--- src/pages/DevelopmentDetails/Balloon.js
+++ src/pages/DevelopmentDetails/Balloon.js
@@ -14,9 +14,15 @@ class Balloon extends Component {
 
         <h2>
           칭찬합시다는 제가 근무하는 회사의 인원이 증가하고 프로젝트 단위로 인원이 구성되다 보니
-          서로가 서로에게 조금 딱딱해진 내부 분위기에 동력이 되었으면 하는 마음에서 기획한 프로젝트 입니다.
-          DevSwag이라는 회사 개발자 발표회에 맞춰 공개하였으며,
-          그 행사에서 칭찬합시다 프로젝트가 2위를 수상하였습니다.
+          조금 딱딱해진 내부 분위기에 많은 분들이 함께 일하는 동료에게 격려나 칭찬 한마디 하는것에 어려움을 느끼는 분들이 많다고 생각했습니다.
+          <br /><br />
+          그래서 서로가 서로에게 보람을 느낄 수 있도록 따뜻한 동력이 되었으면 하는 마음에 기획한 프로젝트 입니다.
+          한달간 자신에게 전달된 메시지는 다음달 1일에 공개하도록 했으며,
+          바로 확인 할 수 있는 메신저가 아닌 편지를 받는 듯한 느낌을 주려고 기획 했습니다.
+          개발된 내용은 DevSwag이라는 사내 개발자 행사에 맞춰 공개하였으며, 그 행사에서 칭찬합시다 프로젝트는 2위를 수상하였습니다.
+
+          <br /><br />
+          매월 1일, 생각지도 않았던 칭찬 메시지에 기분좋은 하루가 되었으면 합니다.
         </h2>
 
         <section className="list-wrap">
@@ -46,6 +52,40 @@ class Balloon extends Component {
 
         <section>
           <h3>Features</h3>
+          <dl>
+            <dt>로그인</dt>
+            <dd>
+              <p>구글 계정 연동하였고 계절 마다 자동으로 테마를 변경 되도록 설정 함</p>
+              <img src={require("statics/img/development/balloon/main.png")} alt={name} />
+            </dd>
+          </dl>
+
+          <dl>
+            <dt>칭찬 메시지 보내기</dt>
+            <dd>
+              <p>매달 7개의 칭찬 풍선이 충전되며, 칭찬 메시지에는 1개의 풍선이 소모 됩니다.</p>
+              <img src={require("statics/img/development/balloon/main.png")} alt={name} />
+            </dd>
+          </dl>
+
+          <dl>
+            <dt>칭찬 메시지 보기</dt>
+            <dd>
+              <p>본인에게 도착한 칭찬 메시지는 매월 1일 칭찬함에서 열람 할 수 있습니다.</p>
+              <img src={require("statics/img/development/balloon/main.png")} alt={name} />
+            </dd>
+          </dl>
+
+          <dl>
+            <dt>칭찬왕 & 칭찬쟁이</dt>
+            <dd>
+              <p>
+                매달 칭찬을 많이 받은 사람과 분기별로 칭찬을 많이 받은 사람을 선별하여<br />
+                휴가를 제공 하자는 제도를 제안 하였고 회사에서도 흔쾌히 수용하여 주었습니다.
+              </p>
+              <img src={require("statics/img/development/balloon/main.png")} alt={name} />
+            </dd>
+          </dl>
         </section>
 
         <section className="more-wrap">
