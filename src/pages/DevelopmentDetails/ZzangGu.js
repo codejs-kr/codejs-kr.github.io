a36e8112fc6c3ad5b293783027dd090dd41ac0b5
dodortus <dodortus@gmail.com>
2019-02-18 23:21:33 +0900

    * Update Development detail content

--- src/pages/DevelopmentDetails/ZzangGu.js
+++ src/pages/DevelopmentDetails/ZzangGu.js
@@ -13,7 +13,10 @@ const ZzangGu = () => {
 
       <section className="overview">
         <p>
-          미니게임
+          대학교 2학년 때 Java Applet을 이용하여 만들었던 저의 첫 게임입니다.<br />
+          당시 이 게임으로 좋은 성적도 받고 처음으로 만든 게임이다 보니 저에게는 소중한 추억이었습니다.<br />
+          웹개발을 하면서 언젠가 당시 게임을 웹 버전으로 구현해 보고 싶다는 생각하였고,
+          오래전 코드를 보며 토이프로젝트로 조금씩 재 구현해 보았습니다.
         </p>
       </section>
 
@@ -21,16 +24,25 @@ const ZzangGu = () => {
         <ul>
           <li>
             <strong>개발년도</strong>
-            <p>진행중</p>
+            <p>2016</p>
           </li>
           <li>
             <strong>역할</strong>
-            <p>웹 어플리케이션 설계 및 개발</p>
+            <p>게임 설계 및 개발</p>
+          </li>
+          <li>
+            <strong>주요개발항목</strong>
+            <p>Canvas 드로잉 처리, 캐릭터 처리, 미사일 처리, 충돌 처리, 키보드 이벤트 처리, 사운드 처리</p>
+          </li>
+          <li>
+            <strong>사용기술</strong>
+            <p>HTML5 Canvas, CSS3, JavaScript(ES6), WebPack</p>
           </li>
         </ul>
       </section>
 
       <section className="button-wrap">
+        <A href="https://github.com/dodortus/zzanggu">Live</A>
         <A href="https://github.com/dodortus/zzanggu" icon="github">Github</A>
       </section>
 
