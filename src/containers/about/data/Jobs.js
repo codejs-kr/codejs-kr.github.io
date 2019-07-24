b5c4757b0a14cfb1eb540c3adceaa74fedfbc1dd
dodortus <dodortus@gmail.com>
2019-07-24 23:47:16 +0900

    * Update TimeLine style

--- src/containers/about/data/Jobs.js
+++ src/containers/about/data/Jobs.js
@@ -1,193 +1,129 @@
 export default [
   {
-    name: "Rsupport",
-    date: "2010.07 ~ 2019.07 현재",
+    name: 'Rsupport',
+    date: '2010.07 ~ 2019.07 현재',
     list: [
       {
-        name: "리모트세미나",
-        role: "웹 프론트엔드 개발",
-        date: "2019.01 ~ 2019",
-        description: "초기 프로토타입 프로젝트를 진행하였습니다.",
-        link: "/#/development/remotemeeting",
-        techStack: [
-          "React",
-          "Redux",
-          "WebRTC",
-          "MQTT",
-          "ES6",
-          "HTML5",
-          "CSS3",
-          "SCSS",
-          "WebPack"
-        ]
+        name: '리모트세미나',
+        role: '웹 프론트엔드 개발',
+        date: '2019.01 ~ 2019',
+        description: 'WebRTC 기반 세미나 서비스의 초기 프로토타입 개발을 진행하였습니다.',
+        link: '/#/development/remotemeeting',
+        techStack: ['React', 'Redux', 'WebRTC', 'MQTT', 'ES6', 'HTML5', 'CSS3', 'SCSS', 'WebPack'],
       },
       {
-        name: "리모트미팅",
-        role: "웹 프론트엔드 개발",
-        date: "2014.09 ~ 2019.07",
-        description: "WebRTC 기반 온라인 화상회의 서비스 설계 및 개발을 담당하였습니다.<br />" +
-        "2014년 WebRTC 기술을 알게 된 후 관련 파일럿 프로젝트를 주도한 것을 시작으로 리모트미팅 프로젝트의 시작을 함께 할 수 있었습니다.",
-        link: "/#/development/remotemeeting",
-        techStack: [
-          "React",
-          "Redux",
-          "WebRTC",
-          "MQTT",
-          "ES6",
-          "HTML5",
-          "CSS3",
-          "SCSS",
-          "WebPack"
-        ]
+        name: '리모트미팅',
+        role: '웹 프론트엔드 개발',
+        date: '2014.09 ~ 2019.07',
+        description:
+          'WebRTC 기반 온라인 화상회의 서비스 설계 및 개발을 담당하였습니다.\n' +
+          '2014년 WebRTC 기술을 알게 된 후 관련 파일럿 프로젝트를 주도한 것을 시작으로 리모트미팅 프로젝트의 시작을 함께 할 수 있었습니다.',
+        link: '/#/development/remotemeeting',
+        techStack: ['React', 'Redux', 'WebRTC', 'MQTT', 'ES6', 'HTML5', 'CSS3', 'SCSS', 'WebPack'],
       },
       {
-        name: "리모트라이브",
-        role: "웹 프론트엔드 개발",
-        date: "2014.05 ~ 2014.08",
-        description: "RemoteLive는 WebRTC 기반의 서비스의 가능성을 검토하기 위한 파일럿 프로젝트였습니다.<br />" +
-          "상용화 된 리모트미팅 프로젝트의 디딤돌이 된 프로젝트이며, Websocket 채팅과 P2P 기반의 화상회의 기능을 모듈화하여 기존 회사 서비스에 적용시키는 방법을 제안한 프로젝트입니다.",
-        link: "/#/development/remotelive",
-        techStack: [
-          "WebRTC",
-          "Node.js",
-          "Socket.io",
-          "ES5",
-          "HTML5",
-          "CSS3",
-          "jQuery",
-          "Gulp"
-        ]
+        name: '리모트라이브',
+        role: '웹 프론트엔드 개발',
+        date: '2014.05 ~ 2014.08',
+        description:
+          'RemoteLive는 WebRTC 기반의 서비스의 가능성을 검토하기 위한 파일럿 프로젝트였습니다.\n' +
+          '상용화 된 리모트미팅 프로젝트의 디딤돌이 된 프로젝트이며, Websocket 채팅과 P2P 기반의 화상회의 기능을 모듈화하여 기존 회사 서비스에 적용시키는 방법을 제안한 프로젝트입니다.',
+        link: '/#/development/remotelive',
+        techStack: ['WebRTC', 'Node.js', 'Socket.io', 'ES5', 'HTML5', 'CSS3', 'jQuery', 'Gulp'],
       },
       {
-        name: "칭찬합시다",
-        role: "기획, 디자인, 프론트, 백엔드 개발",
-        date: "2015 ~ 2014.09",
-        description: "한 달간 자신에게 전달된 메시지는 다음 달 1일에 공개하도록 했으며, 바로 확인할 수 있는 메신저가 아닌 편지를 받는 듯한 느낌을 주려고 기획했습니다.",
-        link: "/#/development/balloon",
+        name: '칭찬합시다',
+        role: '기획, 디자인, 프론트엔드, 백엔드 개발',
+        date: '2015 ~ 2014.09',
+        description:
+          '한 달간 자신에게 전달된 메시지는 다음 달 1일에 공개하도록 했으며, 바로 확인할 수 있는 메신저가 아닌 편지를 받는 듯한 느낌을 주려고 기획했습니다.',
+        link: '/#/development/balloon',
         techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery",
-          "Gulp",
-          "NodeJS",
-          "Express",
-          "EJS",
-          "MongoDB",
-          "Cron",
-          "Nodemon",
-          "Nodemailer"
-        ]
+          'HTML5',
+          'CSS3',
+          'ES5',
+          'jQuery',
+          'Gulp',
+          'NodeJS',
+          'Express',
+          'EJS',
+          'MongoDB',
+          'Cron',
+          'Nodemon',
+          'Nodemailer',
+        ],
       },
       {
-        name: "리모트WOL",
-        role: "웹 프론트엔드 개발",
-        date: "2012 ~ 2014.09",
-        description: "RemoteWOL은 전원이 꺼져 있는 PC의 전원을 원격에서 켤 수 있도록 하는 제품입니다. WOL 기능을 지원하는 하드웨어에 작은 Goahead 웹서버가 구성되었고 그 내부에서 하드웨어 동작을 제어 & 관리하는 싱글 페이지 어플리케이션을 개발하였습니다.",
-        link: "/#/development/remotewol",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery"
-        ]
+        name: '리모트WOL',
+        role: '웹 프론트엔드 개발',
+        date: '2012 ~ 2014.09',
+        description:
+          'RemoteWOL은 전원이 꺼져 있는 PC의 전원을 원격에서 켤 수 있도록 하는 제품입니다. WOL 기능을 지원하는 하드웨어에 작은 Goahead 웹서버가 구성되었고 그 내부에서 하드웨어 동작을 제어 & 관리하는 싱글 페이지 어플리케이션을 개발하였습니다.',
+        link: '/#/development/remotewol',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery'],
       },
       {
-        name: "프론트엔드 컨벤션 사이트 개발",
-        role: "사이트 기획, 설계, 웹 프론트엔드 개발",
-        date: "2012 ~ 2013",
-        description: "자주 사용하는 컴포넌트들의 기본형을 만들기로 시작한 프로젝트이며, 오픈 이후 팀 프로젝트로 발전시킬 수 있었습니다.",
-        link: "/#/development/frontend-convension",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery"
-        ]
+        name: '프론트엔드 컨벤션 사이트 개발',
+        role: '사이트 기획, 설계, 웹 프론트엔드 개발',
+        date: '2012 ~ 2013',
+        description:
+          '자주 사용하는 컴포넌트들의 기본형을 만들기로 시작한 프로젝트이며, 오픈 이후 팀 프로젝트로 발전시킬 수 있었습니다.',
+        link: '/#/development/frontend-convension',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery'],
       },
       {
-        name: "네이트온 내 PC제어",
-        role: "웹 프론트엔드 개발",
-        date: "2012 ~ 2014",
-        description: "내 PC제어 프로젝트는 이미 5년 이상 서비스 중인 서비스를 리뉴얼하는 프로젝트였습니다.",
-        link: "/#/development/remotewol",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery"
-        ]
+        name: '네이트온 내 PC제어',
+        role: '웹 프론트엔드 개발',
+        date: '2012 ~ 2014',
+        description: '내 PC제어 프로젝트는 이미 5년 이상 서비스 중인 서비스를 리뉴얼하는 프로젝트였습니다.',
+        link: '/#/development/remotewol',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery'],
       },
       {
-        name: "삼성 RemotePC",
-        role: "웹 프론트엔드 개발",
-        date: "2011.01 ~ ",
-        description: "삼성 직원들이 자신의 PC를 테블릿에서 원격제어 하기위해 개발된 프로젝트입니다.",
-        link: "/#/development/samsung-remote-pc",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery"
-        ]
+        name: '삼성 RemotePC',
+        role: '웹 프론트엔드 개발',
+        date: '2011.01 ~ ',
+        description: '삼성 직원들이 자신의 PC를 테블릿에서 원격제어 하기위해 개발된 프로젝트입니다.',
+        link: '/#/development/samsung-remote-pc',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery'],
       },
       {
-        name: "리모트뷰 기업용",
-        role: "웹 프론트엔드 개발",
-        date: "2010.08 ~ 2014.09",
-        description: "회사의 주력 제품이며, 재택근무를 가능하게 해주는 유용한 서비스입니다.\n" +
-        "원격지에 설치된 PC의 목록이 노출되며, 해당 PC에 접속하면 전용 뷰어가 나타나며 뷰어를 통해서 원격지의 PC를 제어할 수 있습니다.",
-        link: "/#/development/remoteview-enterprise",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery",
-          "jQuery.ui",
-          "flot.js"
-        ]
+        name: '리모트뷰 기업용',
+        role: '웹 프론트엔드 개발',
+        date: '2010.08 ~ 2014.09',
+        description:
+          '회사의 주력 제품이며, 재택근무를 가능하게 해주는 유용한 서비스입니다.\n' +
+          '원격지에 설치된 PC의 목록이 노출되며, 해당 PC에 접속하면 전용 뷰어가 나타나며 뷰어를 통해서 원격지의 PC를 제어할 수 있습니다.',
+        link: '/#/development/remoteview-enterprise',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery', 'jQuery.ui', 'flot.js'],
       },
       {
-        name: "리모트뷰 개인용",
-        role: "웹 프론트엔드 개발",
-        date: "2010.08 ~ 2014.09",
-        description: "개인 사용자들을 위한 PC원격제어 서비스인 리모트뷰는 제가 담당할 당시 이전 버전 서비스를 완전히 리뉴얼하는 프로젝트였으며, 저의 프론트앤드 개발의 씨앗이 되어준 의미 있는 프로젝트입니다.",
-        link: "/#/development/remoteview-personal",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery",
-          "jQuery mobile",
-          "flot.js"
-        ]
+        name: '리모트뷰 개인용',
+        role: '웹 프론트엔드 개발',
+        date: '2010.08 ~ 2014.09',
+        description:
+          '개인 사용자들을 위한 PC원격제어 서비스인 리모트뷰는 제가 담당할 당시 이전 버전 서비스를 완전히 리뉴얼하는 프로젝트였으며, 저의 프론트앤드 개발의 씨앗이 되어준 의미 있는 프로젝트입니다.',
+        link: '/#/development/remoteview-personal',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery', 'jQuery mobile', 'flot.js'],
       },
       {
-        name: "알서포트닷컴 다국어 리뉴얼",
-        role: "웹 프론트엔드 개발",
-        date: "2010.08 ~ 2011.01",
-        description: "회사 공식 사이트 개발 프로젝트입니다. 5개 국어를 지원하는 사이트로 다국어 처리를 고려하여 UI 요소들을 확장 가능하도록 마크업 하였습니다.",
-        link: "/#/development/rsupport",
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery"
-        ]
+        name: '알서포트닷컴 다국어 리뉴얼',
+        role: '웹 프론트엔드 개발',
+        date: '2010.08 ~ 2011.01',
+        description:
+          '회사 공식 사이트 개발 프로젝트입니다. 5개 국어를 지원하는 사이트로 다국어 처리를 고려하여 UI 요소들을 확장 가능하도록 마크업 하였습니다.',
+        link: '/#/development/rsupport',
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery'],
       },
       {
-        name: "리모트콜",
-        role: "웹 프론트엔드 개발",
-        date: "2010.07 ~ 2011",
-        description: "주로 상담원이 고객의 PC를 제어하는 서비스인 리모트콜의 ASP 유지보수와 서버납품 커스터마이징을 담당하였습니다.",
+        name: '리모트콜',
+        role: '웹 프론트엔드 개발',
+        date: '2010.07 ~ 2011',
+        description:
+          '주로 상담원이 고객의 PC를 제어하는 서비스인 리모트콜의 ASP 유지보수와 서버납품 커스터마이징을 담당하였습니다.',
         link: null,
-        techStack: [
-          "HTML5",
-          "CSS3",
-          "ES5",
-          "jQuery"
-        ]
-      }
-    ]
-  }
+        techStack: ['HTML5', 'CSS3', 'ES5', 'jQuery'],
+      },
+    ],
+  },
 ];
