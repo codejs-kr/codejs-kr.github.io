e72539c2b286039f336dc6adc762ed2a3d8a51c9
dodortus <dodortus@gmail.com>
2019-09-07 19:22:56 +0900

    Deploy code splitting

---
+++ 2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{417:function(e,n,a){"use strict";a.r(n);var t=a(0),l=a.n(t),r=a(7);n.default=function(){return l.a.createElement(r.k,{isSubpage:!0},l.a.createElement(r.j,null,l.a.createElement("span",null,":)"),l.a.createElement("p",null,"잘못된 주소입니다.",l.a.createElement("br",null),"상단 메뉴를 눌러 이동해주세요.")))}}}]);
