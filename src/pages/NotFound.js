9d0f646cc90cd4ad460d3cfb638d0cc3a6624993
dodortus <dodortus@gmail.com>
2019-10-09 13:24:51 +0900

    Update 404 page design

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,11 +5,11 @@ const NotFound = () => {
   return (
     <PageTemplate isSubpage>
       <NotFoundTemplate>
-        <span>:)</span>
+        <h1>404</h1>
         <p>
-          잘못된 주소입니다.
+          찾을 수 없는 페이지입니다.
           <br />
-          상단 메뉴를 눌러 이동해주세요.
+          상단 메뉴를 눌러 이동해주세요 <span>:)</span>
         </p>
       </NotFoundTemplate>
     </PageTemplate>
