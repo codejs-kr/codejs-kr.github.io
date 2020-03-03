aa33b5886bb895ef2c443ef956ab3351f1d51df3
dodortus <dodortus@gmail.com>
2020-03-04 01:21:53 +0900

    Update VideoLoader delay

--- src/components/common/VideoLoader/VideoLoader.js
+++ src/components/common/VideoLoader/VideoLoader.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DefaultLoading } from '../ImageLoader/ImageLoader';
 import './VideoLoader.scss';
 import '../ImageLoader/ImageLoader.scss';
 
-const VideoLoader = ({ src }) => {
+const VideoLoader = ({ src, delay = 500 }) => {
   const [isLoaded, setLoad] = useState(false);
   const className = isLoaded ? 'loaded' : 'loading';
+  const videoEl = React.createRef();
+
+  useEffect(() => {
+    if (isLoaded) {
+      console.log('videoEl', videoEl);
+      setTimeout(() => {
+        if (videoEl.current) {
+          videoEl.current.play();
+        }
+      }, delay);
+    }
+  }, [isLoaded]);
 
   return (
     <>
       {!isLoaded && <DefaultLoading />}
-      <video className={className} autoPlay muted loop onCanPlay={() => setLoad(true)}>
+      <video className={className} muted loop onCanPlay={() => setLoad(true)} ref={videoEl}>
         <source src={src} type="video/mp4" />
         비디오를 지원하지 않습니다.
       </video>
